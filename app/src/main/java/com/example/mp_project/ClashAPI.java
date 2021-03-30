7afed284e9ae07e5ebfd1b343d65907e6919ba6c
LukePower17 <luke.power.17@gmail.com>
2021-03-30 17:12:21 -0400

    fixes main menu screen and adds tab view after entering user id

---
+++ app/src/main/java/com/example/mp_project/ClashAPI.java
@@ -0,0 +1,101 @@
+package com.example.mp_project;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClashAPI extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    public ClashAPI()
+    {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clashapi);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+
+            String id = extras.getString("id");
+        }
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("User Stats");
+        arrayList.add("Clan Stats");
+
+        prepareViewPager(viewPager,arrayList);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    private void prepareViewPager(ViewPager viewPager, ArrayList<String> arrayList) {
+        MainAdapter adapter = new MainAdapter(getSupportFragmentManager());
+
+        UserFragment userFragment = new UserFragment();
+
+        for(int i = 0; i <arrayList.size(); i++)
+        {
+            Bundle bundle = new Bundle();
+            bundle.putString("title", arrayList.get(i));
+            userFragment.setArguments(bundle);
+            adapter.addFragment(userFragment,arrayList.get(i));
+            userFragment = new UserFragment();
+        }
+
+        viewPager.setAdapter(adapter);
+
+    }
+
+    private class MainAdapter extends FragmentPagerAdapter {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        List<Fragment> fragmentList = new ArrayList<>();
+        public void addFragment(Fragment fragment, String title)
+        {
+            arrayList.add(title);
+            fragmentList.add(fragment);
+        }
+
+        public MainAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return arrayList.get(position);
+        }
+    }
+}
